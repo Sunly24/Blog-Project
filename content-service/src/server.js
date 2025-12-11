e4bee345a56a6ab5644ae7cdc0f7d8e0c7f69427
Sunly <sonlychao169@gmail.com>
2025-12-11 19:00:03 +0700

    crud_content_in_content-service

--- content-service/src/server.js
+++ content-service/src/server.js
@@ -1,16 +1,27 @@
-const express = require("express");
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import contentRoute from './routes/contentRoute.js';
 
 const app = express();
 
-// Create an HTTP server
-app.get('/', (req, res) => {
-  res.send('<h1>Hello, Express.js Server!</h1>');
-});
+app.use(bodyParser.json());
+dotenv.config();
 
 // Specify the port to listen on
 const port = process.env.PORT || 3000;
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+// Connect to MongoDB
+const mongourl = process.env.MONGO_URL;
+
+mongoose.connect(mongourl).then(() => {
+  console.log('Connected to MongoDB');
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  })
+}).catch((err) => {
+  console.error('Failed to connect to MongoDB', err);
+})
+
+app.use("/api/content", contentRoute);
